8a13bbeb0e44368c4b424ee10d9d1d0f4120d8b0
Matiaslys <mati1857@edu.easj.dk>
2019-10-08 11:52:58 +0200

    Initial commit

---
+++ src/TestSuite.java
@@ -0,0 +1,12 @@
+public abstract class TestSuite {
+
+    public void runTestSuite(){
+        // TODO
+    }
+
+    protected abstract void testlist();
+
+    public void assertEquals(int expected, int actual) {
+        // TODO
+    }
+}
---
+++ test/TestClass.java
@@ -0,0 +1,15 @@
+public class TestClass extends TestSuite {
+    @Override
+    protected void testlist() {
+        testThis();
+        testThat();
+    }
+
+    public void testThis() {
+        // TODO
+    }
+
+    public void testThat() {
+        //TODO
+    }
+}
